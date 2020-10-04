d840639352d6af5d5016b67e0867ed4c7d5f7f27
david35008 <67857450+david35008@users.noreply.github.com>
2020-10-04 23:13:33 +0300

    update

--- tests/LogReg.test.js
+++ tests/LogReg.test.js
@@ -1,6 +1,5 @@
-const { response } = require('express');
 const request = require('supertest');
-const server = require('../../_cloned-app/app');
+const server = require('../server');
 
 //login logout and register tests
 const userRegisterMock = {
@@ -25,13 +24,13 @@ describe('Register & Login Tests', () => {
     afterAll(async () => {
         await server.close();
     })
-    
+
     // user register
     test('User Can Register', async (done) => {
-        const { body : response } = await request(server)
-        .post('/users/register')
-        .send(userRegisterMock)
-        .expect(201);
+        const { body: response } = await request(server)
+            .post('/users/register')
+            .send(userRegisterMock)
+            .expect(201);
 
         expect(response.message).toBe("Register Success")
         done();
@@ -40,38 +39,58 @@ describe('Register & Login Tests', () => {
     // user login
     test('User Can Login', async (done) => {
         await request(server)
-        .post('/users/register')
-        .send(userLoginMock)
-        .expect(201);
+            .post('/users/register')
+            .send(userLoginMock)
+            .expect(201);
 
         const response = await request(server)
-        .post('/users/login')
-        .send(userLoginMock)
-        .expect(200)
+            .post('/users/login')
+            .send(userLoginMock)
+            .expect(200)
 
         expect(response.body.accessToken.length > 0).toBe(true)
         expect(response.body.refreshToken.length > 0).toBe(true)
         expect(response.body.userName).toBe(userLoginMock.name)
-        done()
-    })  
+        done();
+    })
 
     // user logout
     test('User Can Logout', async (done) => {
         await request(server)
-        .post('/users/register')
-        .send(userLogoutMock)
-        .expect(201);
+            .post('/users/register')
+            .send(userLogoutMock)
+            .expect(201);
 
         const response = await request(server)
-        .post('/users/login')
-        .send(userLogoutMock)
-        .expect(200)
+            .post('/users/login')
+            .send(userLogoutMock)
+            .expect(200)
 
-        const { body : responseOut } = await request(server)
-        .post('/users/logout')
-        .send({token : response.body.refreshToken})
-        .expect(200)
+        const { body: responseOut } = await request(server)
+            .post('/users/logout')
+            .send({ token: response.body.refreshToken })
+            .expect(200)
         expect(responseOut.message).toBe("User Logged Out Successfully")
         done();
+    })
+
+    test('check unknown endpoint handler', async (done) => {
+        await request(server)
+            .delete('/blabla')
+            .expect(404)
+        await request(server)
+            .post('/blabla')
+            .expect(404)
+        await request(server)
+            .get('/blabla')
+            .expect(404)
+        await request(server)
+            .put('/blabla')
+            .expect(404)
+        await request(server)
+            .options('/blabla')
+            .expect(404)
+
+        done()
     })
 })
